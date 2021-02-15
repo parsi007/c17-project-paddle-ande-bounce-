db159798c149fd0fed49d697bde882321c401fd5
parsi007 <76106066+parsi007@users.noreply.github.com>
2021-02-15 14:00:38 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,6 @@
+### Bouncy Balls using Sprites Library in P5 Editor.
+
+#### DESCRIPTION : A Game like Squash which has one ball and a paddle for the player.
+
+#### P5 Editor Link : https://editor.p5js.org/whitehatjr.priya/sketches/4tXb1bAeU
+#### Output Link : https://vishnupriya-whitehatjr.github.io/c17-v2-BouncyPaddle/index.html
---
+++ sketch.js
@@ -0,0 +1,44 @@
+var ball,img,paddle;
+function preload() {
+  ballimg = loadImage('ball.png');
+  paddleimg=loadImage("paddle.png")
+}
+function setup() {
+  createCanvas(400, 400);
+  ball=createSprite(40,200,20,20);
+  ball.addImage (ballimg); 
+  ball.velocityX=9;  
+  paddle=createSprite(350,200,20,100);
+  paddle.addImage(paddleimg)
+  
+
+}
+
+function draw() {
+  background(205,153,0);
+  
+  edges=createEdgeSprites();
+  //Bounce Off the Left Edge only
+  ball.bounceOff(edges[0]); 
+  ball.bounceOff(edges[2]);
+  ball.bounceOff(edges[3]);
+  ball.bounceOff(paddle,explosion);
+  paddle.collide(edges);
+  if(keyDown(UP_ARROW))
+  {
+    paddle.y=paddle.y-20;
+  }
+  
+  if(keyDown(DOWN_ARROW))
+  {
+    paddle.y=paddle.y+20;
+  }
+  drawSprites();
+  
+}
+
+function explosion()
+{
+  ball.velocityY=random(-8,8);
+}
+
